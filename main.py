b47cb4dde6a2b6264e7b3ced8592fa0ef1d72604
theuri96 <wachviqta96@gmail.com>
2026-07-21 14:30:00 +0300

    20th july

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from flask import Flask, render_template
+from database import get_products,get_sales,get_stock
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    x = 5
+    name = "Jane"
+    numbers = [1,2,3,4,5,6,7,8]
+
+    return render_template('index.html',num = x,name = name,numbers = numbers)
 
 @app.route('/products')
 def products():
-    return render_template('products.html')
+    products_data = get_products()
+    return render_template('products.html',products_data = products_data)
 
 
 @app.route('/sales')
 def sales():
-    return render_template('sales.html')
+    sales_data = get_sales()
+    return render_template('sales.html',sales_data = sales_data)
 
 @app.route('/stock')
 def stock():
-    return render_template('stock.html')
+    stock_data = get_stock()
+    return render_template('stock.html',stock_data = stock_data)
 
 @app.route('/dashboard')
 def dashboard():
@@ -32,4 +40,4 @@ def login():
     return render_template('login.html')
 
 
-app.run()
+app.run(debug=True)
